5616dbd3b36e70693e838d1afc6b97b9623024f7
Shewanji <marrionwanjiru26@gmail.com>
2024-07-02 21:34:18 +0300

    let's test

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, request, jsonify
 import requests
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
+weather_api_key = 'd2680916d9ed46a8b11211922240107'
+geolocation_api_key = '7b38a3fffa68f4'
+
+def get_client_ip():
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0]
+    print(f"Client IP: {client_ip}")
+    return client_ip
+
 @app.route('/api/hello', methods=['GET'])
 def hello():
-    visitor_name = request.args.get('visitor_name')
-    
-    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0]
-
-    # Get location and weather
-    weather_api_key = 'd2680916d9ed46a8b11211922240107'
-    geolocation_api_key = 'd8e0ab874e20453a8ce4a790216e9d36'
-
-    get_location = requests.get(f"https://api.ipgeolocation.io/ipgeo?apiKey={geolocation_api_key}")
+    visitor_name = request.args.get('visitor_name', 'visitor')
+    client_ip = get_client_ip()
+    # get location and weather
+    get_location = requests.get(f'https://ipinfo.io/{client_ip}?token={geolocation_api_key}')
     location = get_location.json()
     city = location.get('city')
 
@@ -21,11 +25,11 @@ def hello():
     weather = get_weather.json()
     temp = weather['current']['temp_c']
 
-    # Response in JSON
+    #response in json
     response = {
         'client_ip': client_ip,
         'location': city,
-        'greeting': f'Hello, {visitor_name}! The temperature is {temp} degrees Celsius in {city}.'
+        'greeting': f'Hello, {visitor_name}!, the temperature is {temp} degrees Celsius in {city}'
     }
 
     return jsonify(response)
